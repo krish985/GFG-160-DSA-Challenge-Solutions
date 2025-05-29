d3b3a9b7fed884b5de69db61ce48d74daeb05bad
= <krishkumar9810@gmail.com>
2025-05-29 19:02:12 +0530

    01 : find second largest element in an array

---
+++ ARRAYS/SecondLargest.java
@@ -0,0 +1,40 @@
+package ARRAYS;
+
+import java.util.Arrays;
+
+// Problem: Find the second largest Element in An Array.
+// Link: https://www.geeksforgeeks.org/find-second-largest-element-array/
+// Difficulty: Easy.
+// Companies tag : Sap Labs , TCS , Accenture , Sentinel One.
+
+public class SecondLargest {
+    // Time Complexity : O(n).  // take single parse.
+    // Space Complexity : O(1).
+
+    public static int findSecondLargest(int arr[]){
+        // Edge Case.
+        if(arr.length < 2){
+            return -1; // we have one elemnt second greater ! exist.
+        }
+        // initialize variable largest or secondLargest.
+        int largest = -1;
+        int secondLargest = -1;
+
+        // iterate over the array.
+        for(int i=0; i<arr.length; i++){
+            // if arr[i] > largest update secondlargest = largest.
+            // and largest = curr val.
+            if(arr[i] > largest){
+                secondLargest = largest;
+                largest = arr[i];
+            }else if(arr[i] < largest && arr[i] > secondLargest){
+                // in that case update secondlargest.
+                secondLargest = arr[i];
+            }
+        }
+
+        // return second largest value.
+        return secondLargest;
+    }
+    
+}
